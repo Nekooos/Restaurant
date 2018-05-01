ddfddade3b9dc6e86ec567be5dea3d1df1e2c5c5
Kristoffer <kristofferkramer83@gmail.com>
2018-05-01 20:06:01 +0200

    n

--- restRestaurant/src/main/java/com/rest/practice/repository/MenuItemDao.java
+++ restRestaurant/src/main/java/com/rest/practice/repository/MenuItemDao.java
@@ -2,7 +2,7 @@ package com.rest.practice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.rest.practice.models.MenuItem;
@@ -11,7 +11,7 @@ import com.rest.practice.models.MenuItem;
 public interface MenuItemDao extends JpaRepository<MenuItem, Long> {
 	
 	@Query("SELECT m FROM MenuItem m WHERE id = :menuItemId")
-	MenuItem findMenuItemById(Long menuItemId);
+	MenuItem findMenuItemById(@Param("menuItemId") Long id);
 
 }
  
